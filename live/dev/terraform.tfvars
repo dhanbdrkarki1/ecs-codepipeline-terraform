7711108ff4cbe38c4a871c8d83bf81a5fb8d9b17
dhan-cloudtech <dhan@cloudtechservice.com>
2025-02-21 11:05:15 +0545

    vpc set to two az

--- live/dev/terraform.tfvars
+++ live/dev/terraform.tfvars
@@ -3,7 +3,7 @@
 #================================
 project_name       = "dhan-custom"
 aws_region         = "us-east-2"
-availability_zones = ["us-east-2a"]
+availability_zones = ["us-east-2a", "us-east-2b"]
 environment        = "dev"
 
 
--- live/dev/vpc.tf
+++ live/dev/vpc.tf
@@ -8,8 +8,8 @@ module "vpc" {
   name                       = "vpc"
   availability_zones         = var.availability_zones
   cidr_block                 = "192.168.0.0/16"
-  public_subnet_cidr_blocks  = ["192.168.0.0/23"]
-  private_subnet_cidr_blocks = ["192.168.2.0/24"]
+  public_subnet_cidr_blocks  = ["192.168.0.0/23", "192.168.2.0/23"]
+  private_subnet_cidr_blocks = ["192.168.4.0/24"]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
