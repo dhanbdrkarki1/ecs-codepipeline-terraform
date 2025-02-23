fb0e7849a2026a3ffc508b264a658e854e8598d8
dhan-cloudtech <dhan@cloudtechservice.com>
2025-02-23 11:02:23 +0545

    update log group name

--- live/dev/cloudwatch.tf
+++ live/dev/cloudwatch.tf
@@ -6,11 +6,10 @@ module "cloudwatch_log_groups" {
   for_each = local.log_groups
   create   = true
   # Format: /ecs/<type>/<service-name>/<environment>/<project>
-  name_prefix       = each.value.name_prefix
   name              = "${each.value.name}/${var.environment}/${var.project_name}"
   retention_in_days = each.value.retention_in_days
   custom_tags = {
-    Name        = "${each.value.name_prefix}/${each.value.name}/${var.environment}/${var.project_name}"
+    Name        = "${each.value.name}/${var.environment}/${var.project_name}"
     Environment = var.environment
     Project     = var.project_name
   }
